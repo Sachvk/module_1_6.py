ea39b7c18d3c653f21bdfdae7d10556747aa00a0
Sachvk <serg22011985@gmail.com>
2024-08-07 11:40:06 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Сергей': 1985, 'Андрей': 1988, 'Максим': 2005}
+print(my_dict)
+print(my_dict['Андрей'])
+print(my_dict.get('Анна'))
+my_dict.update({'Тимур' : 1999, 'Юля' : 2007})
+print(my_dict)
+
+my_set = {2024, 7, 8, "Homework", True, 2024, 8}
+print(my_set)
+my_set.update({False, 'Student'})
+print(my_set)
+print(my_set.remove(8))
+print(my_set)
